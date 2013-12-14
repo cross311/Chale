c20af05c28579bfda1f8fdff4b90d895772e880d
Connor Ross <cross@mdsol.com>
2013-12-14 17:03:03 -0500

    starting datalayer

---
+++ GameDataLayer/IRepos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameDataLayer
+{
+    public interface ITournamentRepo
+    {
+        Tournament Get(Guid id);
+
+        IList<Tournament> Get();
+    }
+
+    public interface IGameRepo
+    {
+        Game Get(Guid id);
+
+        IList<Game> GetByTournament(Guid tournamentId);
+    }
+
+    public interface IPlayerRepo
+    {
+        Player Get(Guid id);
+
+        IList<Player> GetByTournament(Guid tournamentId);
+
+        IList<Player> GetByGame(Guid gameId);
+    }
+}
---
+++ GameDataLayer/Models.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GameDataLayer
+{
+    public partial class Tournament
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual DateTime? StartDate { get; set; }
+    }
+
+    public partial class Player
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual Guid TournamentId { get; set; }
+    }
+
+    public partial class Game
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual Guid WinnerId { get; set; }
+
+        public virtual Guid TournamentId { get; set; }
+    }
+}
