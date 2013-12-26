5413b04d1fb05503e5d5c5248061e5c8d22fcfd0
Connor Ross <cross@mdsol.com>
2013-12-26 12:45:39 -0500

    did not completly save all changes

---
+++ Nancy.Web/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿namespace Nancy.Web
+{
+    using GameDataLayer;
+    using Nancy;
+    using Nancy.TinyIoc;
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        // The bootstrapper enables you to reconfigure the composition of the framework,
+        // by overriding the various methods and properties.
+        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            // Autoregister will actually do this for us, so we don't need this line,
+            // but I'll keep it here to demonstrate. By Default anything registered
+            // against an interface will be a singleton instance.
+            container.Register<IRepository<Tournament>, EntityFrameworkRepository<Tournament>>();
+        }
+    }
+}
---
+++ Nancy.Web/Modules/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿namespace Nancy.Web.Modules
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = _ => View["index"];
+        }
+    }
+}
