af1ecbbbc2283cb31d9857aec3f2f986a166f3f4
Connor Ross <cross@mdsol.com>
2013-12-26 12:44:20 -0500

    Switched back to nancy

--- GameDataLayer/IRepos.cs
+++ GameDataLayer/IRepos.cs
@@ -4,10 +4,32 @@ using System.Linq;
 
 namespace GameDataLayer
 {
-    public interface IRepository<T>
+    public interface IRepository<T> where T : class
     {
         IQueryable<T> Get { get; }
 
         T Save(T value);
+    }
+
+    public class EntityFrameworkRepository<T> : IRepository<T> where T : class
+    {
+        private TournamentContext _Context;
+
+        public EntityFrameworkRepository(TournamentContext context)
+        {
+            _Context = context;
+        }
+
+        IQueryable<T> IRepository<T>.Get
+        {
+            get { return _Context.Set<T>(); }
+        }
+
+        public T Save(T value)
+        {
+            var addedValue = _Context.Set<T>().Add(value);
+            _Context.SaveChanges();
+            return addedValue;
+        }
     }
 }
--- Nancy.Web/Properties/AssemblyInfo.cs
+++ Nancy.Web/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Web")]
+[assembly: AssemblyTitle("Nancy.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Web")]
+[assembly: AssemblyProduct("Nancy.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e4ceea74-bc3f-4dfb-a9d6-6a38b6a01c96")]
+[assembly: Guid("3d2e7c4d-8bdf-4bf4-a734-506982528c1e")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
